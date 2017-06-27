68ca92b64fe48235e63c8f5decb59c80bb4a8bbe
Vladimir Eryshev <vladimir.eryshev@wukkerteam.com>
2017-06-27 19:22:52 +0300

    Initial Commit

---
+++ RSSReaderTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RSSReaderTest
+//
+//  Created by VLAD on 27/06/2017.
+//  Copyright © 2017 Vlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
